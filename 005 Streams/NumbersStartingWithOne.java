4f153c35345b5393b19930ad059f3244ee18b4b5
Shruti Mittu <shruti.mittu@gmail.com>
2025-10-06 04:51:29 -0400

    Update NumbersStartingWithOne.java

--- 005 Streams/NumbersStartingWithOne.java
+++ 005 Streams/NumbersStartingWithOne.java
@@ -8,8 +8,8 @@ class NumbersStartingWithOne {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(11, 13, 14, 24, 30);
         System.out.println("Elements in list are: " + numbers);
-        Set<Integer> startingWithOne;
-        startingWithOne = numbers.stream().filter(number -> String.valueOf(number).toString().startsWith("1")).collect(Collectors.toSet()); 
+        Set<Integer> startingWithOne = numbers.stream().filter(number -> String.valueOf(number).toString().startsWith("1"))
+            .collect(Collectors.toSet()); 
         System.out.println("Duplicate Elements in given list are: " + startingWithOne);
     }
 }
